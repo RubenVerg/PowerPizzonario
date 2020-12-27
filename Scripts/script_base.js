bf8319eff670de7b7a009b0f2c7733c2fd5c652d
Giovanni Flor <31931819+GioFlor@users.noreply.github.com>
2020-12-27 23:33:10 +0100

    Fix Grafici
    Fix grafici vari
    Continuazione grafica Nickquotes

--- Scripts/script_base.js
+++ Scripts/script_base.js
@@ -1,15 +1,15 @@
-// Change style of navbar on scroll
-window.onscroll = function() {myFunction()};
-function myFunction() {
+// Cambia lo stile della navbar quando si scrolla
+window.onscroll = function() {ScrollNavbar()};
+function ScrollNavbar() {
     var navbar = document.getElementById("myNavbar");
     if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-        navbar.className = "w3-bar" + " w3-card" + " w3-animate-top" + " w3-white";
+        navbar.className = "w3-bar" + " w3-card" + " w3-animate-top" + " w3-black";
     } else {
-        navbar.className = navbar.className.replace(" w3-card w3-animate-top w3-white", "");
+        navbar.className = navbar.className.replace("w3-card w3-animate-top w3-black", "");
     }
 }
 
-// Used to toggle the menu on small screens when clicking on the menu button
+// Apre e chiude il menu quando lo schermo è piccolino
 function toggleFunction() {
     var x = document.getElementById("navDemo");
     if (x.className.indexOf("w3-show") == -1) {
@@ -19,6 +19,7 @@ function toggleFunction() {
     }
 }
 
+//Aprono o chiudono la barra nella sezione dei credits
 function apriNav() {
   document.getElementById("mySidebar").style.width = "60%";
   document.getElementById("mySidebar").style.display = "block";
@@ -26,4 +27,20 @@ function apriNav() {
 
 function chiudiNav() {
   document.getElementById("mySidebar").style.display = "none";
+}
+
+//Bottoni Nickquotes
+function SceltaRandom()
+{
+  alert("ciaone randome");
+}
+
+function SceltaTutte()
+{
+  var tabellaQuote = document.getElementById('stampaNickquotes');
+  if (tabellaQuote.style.display === 'none') {
+    tabellaQuote.style.display = 'block';
+  } else {
+    tabellaQuote.style.display = 'none';
+  }
 }
