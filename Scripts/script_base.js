ae4bc43544079aef83790dea4bca7b1994d1b116
GioRolf <31931819+GioRolf@users.noreply.github.com>
2021-01-20 16:55:46 +0100

    Aggiunta delle nuove API e dell'Alfabeto
    - Aggiunte le nuove API per la chiamata al file sheet
    - Aggiunto l'alfabeto nelle varie pagine
    - Sistemata la navbar
    - Aggiunte immagini dei banner
    - Vari fix grafici

--- Scripts/script_base.js
+++ Scripts/script_base.js
@@ -1,14 +1,3 @@
-// Cambia lo stile della navbar quando si scrolla
-window.onscroll = function() {ScrollNavbar()};
-
-function ScrollNavbar() {
-    var navbar = document.getElementById("myNavbar");
-    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-        navbar.className = "w3-bar" + " w3-card" + " w3-animate-top" + " w3-black";
-    } else {
-        navbar.className = navbar.className.replace("w3-card w3-animate-top w3-black", "");
-    }
-}
 
 // Apre e chiude il menu quando lo schermo è piccolino
 function toggleFunction() {
@@ -20,7 +9,7 @@ function toggleFunction() {
     }
 }
 
-//Aprono o chiudono la barra nella sezione dei credits
+//Aprono o chiudono la barra nella pagina dei credits
 function apriNav() {
   document.getElementById("mySidebar").style.width = "100%";
   document.getElementById("mySidebar").style.display = "block";
@@ -31,8 +20,7 @@ function chiudiNav() {
 }
 
 // Bottone per tutte le Nickquotes
-function SceltaTutte()
-{
+function SceltaTutte() {
   var tabellaQuote = document.getElementById('stampaNickquotes');
   var btnAllQuote = document.getElementById('btnvoci');
 
@@ -49,40 +37,29 @@ function SceltaTutte()
   }
 }
 
-// Bottone per la Nickquote randomica
-function SceltaRandom()
-{
-  var randomQuote = document.getElementById('randomNickquotes');
-  var btnRandomQuote = document.getElementById('btnrandom');
-  var numQuotes = 69;   //Modificare questa a seconda di quante quotes ci sono
+// Bottone per tutte le Nickquotes
+function SceltaRandom(){
+  var divRandom = document.getElementById('divRandomQuote');
+  var btnRandom = document.getElementById('btnrandom');
 
-  //Controllo per mostrare o nascondere la sezione della quote
-  if (randomQuote.style.display === 'none')
-  {
-    // Se il div è nascosto, allora fa tutto il processo per generare l'URL nuovo
-    // per l'estrapolazione della quote
-    // Dichiarazioni delle variabili
-    var quoteScelta = Math.floor((Math.random() * numQuotes)+1);
-
-    // Variabile per l'id della quote da mostrare
-    var stringaUrl = "?ID=";
-    stringaUrl += quoteScelta;
-
-    // Genera un nuovo URL
-    var nuovoUrl = window.location.protocol + "//" + window.location.host + window.location.pathname + stringaUrl;
-    window.history.replaceState(null , null , nuovoUrl);
-
+  if (divRandom.style.display === 'none'){
     // Cambia il testo del bottone
-    btnRandomQuote.innerHTML = "Nascondi la Nickquote!";
-    // Mostra la quote random
-    randomQuote.style.display = 'block';
-
-    location.reload();
-  }
-  else{
+    btnRandom.innerHTML = "Nascondi la Nickquote!";
+    // Mostra la tabella delle quotes
+    divRandom.style.display = 'block';
+  } else {
     // Cambia il testo del bottone
-    btnRandomQuote.innerHTML = "Beccati una quote random!";
-    // Nasconde la quote random
-    randomQuote.style.display = 'none';
+    btnRandom.innerHTML = "Beccati una quote random!";
+    // Nasconde la tabella delle quotes
+    divRandom.style.display = 'none';
   }
 }
+
+// Bottone in fondo a destra: inizializzazione
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.fixed-action-btn');
+  var instances = M.FloatingActionButton.init(elems, {
+    direction: 'left',
+    hoverEnabled: false,
+  });
+});
--- Scripts/script_controllo.js
+++
@@ -1,7 +0,0 @@
-const urlParams = new URLSearchParams(window.location.search);
-const myParam = urlParams.get('ID');
-
-if (myParam != null) {
-  var randomQuote = document.getElementById('randomNickquotes');
-  randomQuote.style.display = 'block';
-}
